cd4e29ba68d2c3fba31cda318c3e0a0679c0faa8
salman-ctrl <zirmanvictory@gmail.com>
2025-03-09 22:06:49 +0700

    edit style

--- src/component/LandingPage.jsx
+++ src/component/LandingPage.jsx
@@ -20,12 +20,12 @@ const LandingPage = () => {
             <img
                 src={background}
                 alt="Background"
-                className={`w-screen h-screen transition-all duration-1000 ${loaded ? "blur-0 opacity-100" : "blur-lg opacity-0"
+                className={`w-screen sm:h-screen  h-[60vh] transition-all  duration-1000 ${loaded ? "blur-0 opacity-100" : "blur-lg opacity-0"
                     }`}
             />
             <div className="h-screen relative w-screen bg-cover bg-center flex flex-col" style={{ backgroundImage: `url(${bg})` }}>
 
-                <div className="w-full h-full flex justify-center items-center">
+                <div className="w-full h-full flex justify-center items-center ">
                     <motion.div
                         initial={{ opacity: 0, scale: 0.5 }}
                         animate={{ opacity: 1, scale: 1 }}
@@ -50,7 +50,10 @@ const LandingPage = () => {
                         initial={{ opacity: 0, scale: 1 }}
                         animate={{ opacity: 1, scale: 1 }}
                         transition={{ duration: 1 }}
-                        className="w-[700px] h-[500px] absolute top-[50%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-2xl shadow-lg overflow-hidden"
+                        className="absolute top-[50%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 
+                              w-[90%] sm:w-[600px] md:w-[700px] 
+                              h-auto min-h-[300px] sm:h-[400px] md:h-[500px] 
+                              bg-white rounded-2xl shadow-lg overflow-hidden"
                     >
                         <div className="p-4 flex justify-between items-center border-b">
                             <h2 className="text-lg font-semibold font-[Poppins]">Mengenal Warisan Budaya</h2>
@@ -61,18 +64,20 @@ const LandingPage = () => {
                                 <MdOutlineClose />
                             </button>
                         </div>
-                        <iframe
-                            width="100%"
-                            height="100%"
-                            src="https://www.youtube.com/embed/BDy2ChJusqU?autoplay=1"
-
-                            title="Mengenal Warisan Budaya"
-                            frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen
-                            className="w-full h-full border-2"
-                        />
+                        <div className="w-full aspect-video">
+                            <iframe
+                                width="100%"
+                                height="100%"
+                                src="https://www.youtube.com/embed/BDy2ChJusqU?autoplay=1"
+                                title="Mengenal Warisan Budaya"
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                                className="w-full h-full border-2"
+                            />
+                        </div>
                     </motion.div>
+
                 )}
             </div>
 
