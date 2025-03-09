c063e407d8231531f4f6ab78eb9e6905d5055aa1
salman-ctrl <zirmanvictory@gmail.com>
2025-03-09 14:02:06 +0700

    first commit

--- src/component/Link.jsx
+++ src/component/Link.jsx
@@ -7,7 +7,7 @@ const Link = () => {
             <img src={Links} alt="" className="w-full h-full object-cover" />
             <div className="w-96 h-52 hover:scale-110 transition-all duration-700 bg-blue-500 flex items-center justify-evenly flex-col absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4 rounded-lg shadow-lg">
                 <p className="text-white text-[20px] font-bold">Siap Tantangan? Klik di Bawah!</p>
-                <button className="bg-white text-blue-500 hover:bg-blue-500 hover:text-white px-4 py-2 rounded-md font-semibold hover:border hover:border-gray-200 hover:shadow hover:scale-105 transition-all duration-500">
+                <button className="bg-white text-blue-500 hover:bg-blue-500 hover:text-white px-5 py-2 rounded-md font-semibold hover:border hover:border-gray-200 hover:shadow hover:scale-105 transition-all duration-500">
                     <a href="https://kahoot.it/">Mulai!</a>
                 </button>
             </div>
